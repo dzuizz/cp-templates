9a7822eb6e5a689c54b8197fc0c784df82fb30cb
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2025-03-12 23:39:10 +0800

    Refactored LCA w/ Binary Lifting Code

--- 3. Trees/LCA with Binary Lifting.cpp
+++ 3. Trees/LCA with Binary Lifting.cpp
@@ -1,68 +1,37 @@
-// Lowest Common Ancestor (LCA) with Binary Lifting
 // https://cses.fi/problemset/task/1688/
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 #define int long long
-
-const int MAXN = 2e5+5;
-const int LOG = 18;
-
-vector<int> adj[MAXN];
-int pa[MAXN][LOG], dep[MAXN];
-
-void dfs(int cur) {
-    for (auto &nx : adj[cur]) {
-        dep[nx] = dep[cur] + 1;
-        dfs(nx);
-    }
-}
-
+const int MAXN=2e5;
+const int LOG=18;
+int n, q, pa[MAXN][LOG], dep[MAXN];
 int lca(int a, int b) {
-    if (dep[a] < dep[b]) swap(a, b);
-    for (int i=LOG-1; i>=0; i--) {
-        if (dep[pa[a][i]] >= dep[b]) {
-            a = pa[a][i];
-        }
-    }
+  if(dep[a] > dep[b])
+    swap(a, b);
+  for(int j=LOG-1; j>=0; --j) if(dep[pa[b][j]] >= dep[a])
+    b = pa[b][j];
+  if(a == b) return a;
+  for(int j=LOG-1; j>=0; --j) if(pa[a][j] != pa[b][j])
+    a = pa[a][j], b = pa[b][j];
+  return pa[a][0];
+}
+signed main(){
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  cin >> n >> q;
+  for(int i=1; i<n; ++i) {
+    int e; cin >> e;
+    pa[i][0] = --e;
+    dep[i] = dep[e] + 1;
+  }
 
-    if (a == b) return a;
+  for(int j=0; j<LOG-1; ++j) for(int i=0; i<n; ++i)
+    pa[i][j+1] = pa[pa[i][j]][j];
 
-    for (int i=LOG-1; i>=0; i--) {
-        if (pa[a][i] != pa[b][i]) {
-            a = pa[a][i];
-            b = pa[b][i];
-        }
-    }
-    return pa[a][0];
+  while(q--) {
+    int a, b; cin >> a >> b;
+    cout << lca(--a, --b) + 1 << '\n';
+  }
+  return 0;
 }
 
-signed main() {
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    int n, q; cin >> n >> q;
-
-    dep[1] = 0, pa[1][0] = 1;
-    for (int i=2; i<=n; i++) {
-        cin >> pa[i][0];
-        adj[pa[i][0]].push_back(i);
-    }
-
-    dfs(1);
-
-    for (int i=1; i<=n; i++) {
-        for (int j=1; j<LOG; j++) {
-            pa[i][j] = pa[pa[i][j-1]][j-1];
-        }
-    }
-    
-    while (q--) {
-        int a, b; cin >> a >> b;
-
-        cout << lca(a, b) << '\n';
-    }
-}
-
-// Time complexity: O(NlogN + QlogN)
-// N = number of nodes
-// Q = number of queries
